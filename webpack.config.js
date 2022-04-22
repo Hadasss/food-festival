d61fb585136a62ac52f3cb2b5f9428b58fb6ea08
Hadasss <hadas.gadish@gmail.com>
2022-04-22 15:22:29 -0700

    modularized code

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,16 @@
 const path = require("path");
 const webpack = require("webpack");
+const BundleAnalyzerPlugin =
+  require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 module.exports = {
   entry: "./assets/js/script.js",
   output: { path: path.resolve(__dirname, "dist"), filename: "main.bundle.js" },
-  plugins: [new webpack.ProvidePlugin({ $: "jquery", jQuery: "jquery" })],
+  plugins: [
+    new webpack.ProvidePlugin({ $: "jquery", jQuery: "jquery" }),
+    // the report outputs to an HTML file in the dist folder:
+    new BundleAnalyzerPlugin({ analyzerMode: "static" }),
+  ],
+
   mode: "development",
 };
